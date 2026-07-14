78b89bf2bc3aeb3c54dde7250ad91a61e27a4fe0
developerpuspendu <bhaduripuspendu786@outlook.com>
2026-07-14 10:09:15 +0530

    initiakization done

---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+function Todos() {
+    return (
+        <div>
+            <h1>Todos</h1>
+            <ul>
+                <li>Todo 1</li>
+                <li>Todo 2</li>
+                <li>Todo 3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Todos;
